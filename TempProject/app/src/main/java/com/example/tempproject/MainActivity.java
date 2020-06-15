1db4981df20227c81db23c47cace577e766783e6
Jaeho <49357353+jeaho0613@users.noreply.github.com>
2020-06-15 15:15:37 +0900

    -restart

---
+++ TempProject/app/src/main/java/com/example/tempproject/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.tempproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String str = intent.getExtras().getString("키");
+        EditText editText = findViewById(R.id.edit_text);
+        editText.setText(str);
+
+    }
+
+
+
+}
